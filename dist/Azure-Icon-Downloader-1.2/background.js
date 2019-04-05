9de1fc07440993532443ff370c4ca38cfb8ab36d
Matt LaGrandeur <mattlag@users.noreply.github.com>
2019-04-05 14:28:47 -0700

    version 1.2

---
+++ dist/Azure-Icon-Downloader-1.2/background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(function() {
+	// Replace all rules ...
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		// With a new rule ...
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				// That fires when a page's URL contains:
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: { urlContains: 'portal.azure.com' },
+					})
+				],
+				// And shows the extension's page action.
+				actions: [ new chrome.declarativeContent.ShowPageAction() ]
+			}
+		]);
+	});
+});

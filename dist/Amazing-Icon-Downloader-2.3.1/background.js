206a6a4d93440bb034cbb7acfc19e01a218f8de4
Matt LaGrandeur <mattlag@gmail.com>
2026-07-02 13:49:02 -0700

    marketplace files for v2.3.1

---
+++ dist/Amazing-Icon-Downloader-2.3.1/background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(function() {
+	// Replace all rules ...
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		// With a new rule ...
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				// That fires when a page's URL contains:
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: { urlMatches: '(portal.azure.com|endpoint.microsoft.com)' },
+					})
+				],
+				// And shows the extension's page action.
+				actions: [ new chrome.declarativeContent.ShowAction() ]
+			}
+		]);
+	});
+});
